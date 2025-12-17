434cfe7c993eaf81bcaa06c4b27ef3431fea6a86
KhadijaMammadliFC <xedice.memmedli@ferrumcapital.az>
2025-12-17 05:43:12 -0800

    redux started

---
+++ React-redux-toolkit/src/redux/components/Counter.jsx
@@ -0,0 +1,10 @@
+import {useSelector, useDispatch} from "react-redux"
+
+export default function Counter() {
+    const count = useSelector((state)=>state.value)
+  return (
+    <>
+
+    </>
+  )
+}
